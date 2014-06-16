95c0eaa0e367956950f211247166148530f5775a
gayathma <gayathma3@gmail.com>
2014-06-16 10:35:03 +0530

    corrections

--- application/controllers/employee/employee_profile_controller.php
+++ application/controllers/employee/employee_profile_controller.php
@@ -1,40 +1,36 @@
 <?php
 
-if(!defined('BASEPATH'))
+if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Employee_profile_controller extends CI_Controller{
-    
-    function __construct(){
+class Employee_profile_controller extends CI_Controller {
+
+    function __construct() {
         parent::__construct();
-        
-    if (!$this->session->userdata('EMPLOYEE_LOGGED_IN')){
-        redirect(site_url().'login/login_controller');
-        
+
+        if (!$this->session->userdata('EMPLOYEE_LOGGED_IN')) {
+            redirect(site_url() . 'login/login_controller');
+        } else {
+
+            $this->load->model('employee/employee_model');
+            $this->load->model('employee/employee_service');
+        }
     }
     
- else {
-     
-        $this->load->model('employee/employee_model');
-        $this->load->model('employee/employee_service');
-        
+
+    function view_profile() {
+
+        $employee_service = new Employee_service();
+
+        $data['heading'] = "My Profile";
+        $data['employee_detail'] = $employee_service->get_employee_by_id($this->session->userdata('EMPLOYEE_CODE'));
+
+
+
+        $partials = array('content' => 'employee/employee_profile_view');
+        $this->template->load('template/main_template', $partials, $data);
     }
-        
-}
 
-function view_profile(){
-    
-    $employee_service = new Employee_service();
-    
-    $data['heading']="View Profile";
-    $data['employee_detail']= $employee_service->get_employee_by_id($this->session->userdata('EMPLOYEE_CODE'));
-
-
-
-  $partials= array('content'=>'employee/manage_employee_view');
-  $this->template->load('template/main_template',$partials,$data);
-  
-}
 }
 
 ?>
