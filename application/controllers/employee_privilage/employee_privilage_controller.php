eecf3e0f2a138cafdc1b2ea652d3426ece300b46
dahami <dahamiyh@ymail.com>
2014-06-19 14:04:57 +0100

    employee_privilage_controller

--- application/controllers/employee_privilage/employee_privilage_controller.php
+++ application/controllers/employee_privilage/employee_privilage_controller.php
@@ -1,8 +1,33 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Employee_privilage_controller extends CI_Controller {
+   
+    function __construct() {
+        parent::__construct();
+
+        if (!$this->session->userdata('EMPLOYEE_LOGGED_IN')) {
+            redirect(site_url() . 'login/login_controller');
+        } else {
+
+            $this->load->model('employee_privilage/employee_privilage_model');
+            $this->load->model('employee_privilage/employee_privilage_service');
+        }
+    }
+    
+   function add_employee_privilage() {
+      
+        $employee_privilage_model = new employee_privilage_model();
+        $employee_privilage_service = new employee_privilage_service();
+        
+        $employee_privilage_model->set_employee_privilage_code($this->input->post('employee_privilage_code',TRUE));
+        $employee_privilage_model->set_employee_code($this->input->post('employee_code', TRUE));
+        $employee_privilage_model->set_privilage_code($this->input->post('privilage_code', TRUE));
+        $employee_privilage_model->set_added_date(date("Y-m-d H:i:s"));
+        
+        echo $employee_service->add_new_employee_privilage($employee_privilage_model);
+   }
+}
 
