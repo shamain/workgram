3db1e97a56593d3e25ee891601fe13d4723a4710
rachini <rachini94perera@gmail.com>
2014-06-20 13:05:54 +0530

    employee_task

---
+++ application/models/employee_task/employee_task_model.php
@@ -0,0 +1,47 @@
+<?php
+
+class Employee_task_model extends CI_Model{
+    
+    var $employee_task_id;
+    var $employee_id;
+    var $task_id;
+    var $added_date;
+    
+    function __construct() {
+        parent::__construct();
+    }
+    
+    public function get_employee_task_id(){
+        return $this->employee_task_id ;
+        
+    }
+    
+    public function get_employee_id(){
+        return $this->employee_id;
+    }
+    
+    public function get_task_id(){
+        return $this->task_id;
+    }
+    
+    public function get_added_date(){
+        return $this->added_date;
+    }
+    
+    public function set_employee_task_id($employee_task_id){
+        $this->employee_task_id=$employee_task_id;
+    }
+    
+    public function set_employee_id($employee_id){
+        $this->employee_id=$employee_id;
+    }
+    
+    public function set_task_id($task_id){
+        $this->task_id=$task_id;
+                
+    }
+    
+    public function set_added_date($added_date){
+        $this->added_date=$added_date;
+    }
+}
---
+++ application/models/employee_task/employee_task_service.php
@@ -0,0 +1,10 @@
+<?php
+
+class Employee_task_service extends CI_Model{
+    
+    function __construct() {
+        parent::__construct();
+    }
+    
+    
+}
