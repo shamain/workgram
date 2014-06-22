cdf0080da582e2f77889ad8e4a8d3832d2a2b3f0
rachini <rachini94perera@gmail.com>
2014-06-22 12:45:22 +0530

    employee privilege view

--- application/views/employee_privilege/manage_employee_privilege_view.php
+++ application/views/employee_privilege/manage_employee_privilege_view.php
@@ -1,6 +1,21 @@
 <div class="page-title">	
-    <h3><?php echo $heading; ?></h3>		
+    <h3><?php echo $heading.'- '. ucfirst($employee_detail->employee_fname) ?><span class="semi-bold"><?php echo ucfirst($employee_detail->employee_lname); ?></h3>		
 </div>
+
+<?php
+
+foreach ($employees as $employee) {
+                            ?> 
+             
+      <button type="button" class="btn btn-default" data-dismiss="modal">ADMIN</button>
+      <button type="button" class="btn btn-default" data-dismiss="modal">EMPLOYEE</button>
+      <button type="button" class="btn btn-default" data-dismiss="modal">COMPANY_OWNER</button>
+      
+      
+
+      
+
+          
 
 
 
