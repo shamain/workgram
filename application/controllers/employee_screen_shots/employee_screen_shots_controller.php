c294c11c82e3a06d42088b9664117c98d676a563
gayathma <gayathma3@gmail.com>
2014-07-17 20:46:30 +0530

    corrections

--- application/controllers/employee_screen_shots/employee_screen_shots_controller.php
+++ application/controllers/employee_screen_shots/employee_screen_shots_controller.php
@@ -4,7 +4,7 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Employee_screen_shot_controller extends CI_Controller {
+class Employee_screen_shots_controller extends CI_Controller {
 
     function __construct() {
         parent::__construct();
@@ -12,19 +12,19 @@ class Employee_screen_shot_controller extends CI_Controller {
         if (!$this->session->userdata('EMPLOYEE_LOGGED_IN')) {
             redirect(site_url() . 'login/login_controller');
         } else {
-
-            $this->load->model('employee_screen_shot/employee_screen_shot_model');
-            $this->load->model('employee_screen_shot/employee_screen_shot_service');
+//
+//            $this->load->model('employee_screen_shot/employee_screen_shot_model');
+//            $this->load->model('employee_screen_shot/employee_screen_shot_service');
         }
     }
     function manage_employee_screen_shot() {
 
-        $employee_screen_shot_service = new employee_screen_shot_service();
+//        $employee_screen_shot_service = new employee_screen_shot_service();
 
         $data['heading'] = "Manage Employee_screen_shot_service";
-        $data['employee_screen_shot'] = $employee_screen_shot_service->get_employee_screen_shot_by_screen_shot_id($this->session->userdata(''));
+//        $data['employee_screen_shot'] = $employee_screen_shot_service->get_employee_screen_shot_by_screen_shot_id($this->session->userdata(''));
 
-        $partials = array('content' => 'employee_screen_shot/manage_employee_screen_shot_view');
+        $partials = array('content' => 'employee_screen_shots/screen_shot_view');
         $this->template->load('template/main_template', $partials, $data);
     }
 
