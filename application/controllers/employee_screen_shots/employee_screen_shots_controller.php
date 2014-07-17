85b781723e8dbdbed1c4a839cb2136b24c28394e
dahami <dahamiyh@ymail.com>
2014-07-17 16:08:22 +0100

    employee screen shot controller 1

--- application/controllers/employee_screen_shots/employee_screen_shots_controller.php
+++ application/controllers/employee_screen_shots/employee_screen_shots_controller.php
@@ -1,8 +1,41 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Employee_screen_shot_controller extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+
+        if (!$this->session->userdata('EMPLOYEE_LOGGED_IN')) {
+            redirect(site_url() . 'login/login_controller');
+        } else {
+
+            $this->load->model('employee_screen_shot/employee_screen_shot_model');
+            $this->load->model('employee_screen_shot/employee_screen_shot_service');
+        }
+    }
+    function manage_employee_screen_shot() {
+
+        $employee_screen_shot_service = new employee_screen_shot_service();
+
+        $data['heading'] = "Manage Employee_screen_shot_service";
+        $data['employee_screen_shot'] = $employee_screen_shot_service->get_employee_screen_shot_by_screen_shot_id($this->session->userdata(''));
+
+        $partials = array('content' => 'employee_screen_shot/manage_employee_screen_shot_view');
+        $this->template->load('template/main_template', $partials, $data);
+    }
+
+    
+    
+    
+    
+
+    
+    
+    
+    
+}
 
