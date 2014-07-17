884ded7b664b065830123d01b710dded3b7085c7
dahami <dahamiyh@ymail.com>
2014-07-17 17:16:21 +0100

    sacreen_shot_view 2

--- application/views/employee_screen_shots/screen_shot_view.php
+++ application/views/employee_screen_shots/screen_shot_view.php
@@ -1,45 +1,48 @@
 <?php
 
-$sThumbTemplate = <<<HTML
-<li><a href="#" rel="nofollow" title="{title}"><img src="{fileurl}" alt="{title}" /></a></li>
-HTML;
+<div class="superbox">
+        <div class="superbox-list"> <img src="" data-img="" alt="" class="superbox-img"> </div>
+        
+        <div class="superbox-list"> <img src="" data-img="" alt="" class="superbox-img"> </div>
+        
+        <div class="superbox-list"> <img src="" data-img="" alt="" class="superbox-img"> </div>
+       
+        <div class="superbox-list"> <img src="" data-img="" alt="" class="superbox-img"> </div>
+        
+        <div class="superbox-list"> <img src="" data-img="" alt="" class="superbox-img"> </div>
+        
+        <div class="superbox-list"> <img src="" data-img="" alt="" class="superbox-img"> </div>
+        
+        <div class="superbox-list"> <img src="" data-img="" alt="" class="superbox-img"> </div>
+        
+        <div class="superbox-list"> <img src="" data-img="" alt="" class="superbox-img"> </div>
+     
+        <div class="superbox-list"> <img src="" data-img="" alt="" class="superbox-img"> </div>
+        
+        <div class="superbox-list"> <img src="" data-img="" alt="" class="superbox-img"> </div>
+        
+        <div class="superbox-list"> <img src="" data-img="" alt="" class="superbox-img"> </div>
+      
+        <div class="superbox-list"> <img src="" data-img="" alt="" class="superbox-img"> </div>
+       
+        <div class="superbox-list"> <img src="" data-img="" alt="" class="superbox-img"> </div>
+        
+        <div class="superbox-list"> <img src="" data-img="" alt="" class="superbox-img"> </div>
+        
+        
 
- $sImageTemplate = <<<HTML
-<div class="sliderkit-panel">
-    <img src="{fileurl}" alt="{title}" />
-    <div class="sliderkit-panel-textbox">
-        <div class="sliderkit-panel-text">
-            <h4>{title}</h4>
-            <p>{description}</p>
-        </div>
-       <div class="sliderkit-panel-overlay"></div>
-  </div>
-</div>
-HTML;
-
-$sThumbs = $sImages = '';
-$sFolder = 'data_images/';
-
- $aUnits = array(
-
-    'pic1.jpg' => 'Image 1', 'pic2.jpg' => 'Image 2', 'pic3.jpg' => 'Image 3', 'pic4.jpg' => 'Image 4',
-    'pic5.jpg' => 'Image 5', 'pic6.jpg' => 'Image 6', 'pic7.jpg' => 'Image 7', 'pic8.jpg' => 'Image 8',
-    'pic9.jpg' => 'Image 9', 'pic10.jpg' => 'Image 10'
-);
-
-foreach ($aUnits as $sFileName => $sTitle) {
-    $sThumbs .= strtr($sThumbTemplate, array('{fileurl}' => $sFolder . 't_' . $sFileName, '{title}' => $sTitle));
-    $sImages .= strtr($sImageTemplate, array('{fileurl}' => $sFolder . $sFileName, '{title}' => $sTitle, '{description}' => $sTitle . ' photo description here'));
-    }
-
+        <div class="superbox-float"></div>
+      </div>
+        
  
 
-header("Content-Type: application/json");
-require_once('Services_JSON.php');
-$oJson = new Services_JSON();
-echo $oJson->encode(array('thumbs' => $sThumbs, 'images' => $sImages));
-
+<script>
+		$(function() {		
+			// Call SuperBox - that's it!
+			$('.superbox').SuperBox();		
+		});
+		</script>
+                
  
-
 ?>
 
