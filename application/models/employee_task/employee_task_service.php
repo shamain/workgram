45db04f44a1ea2fa520be89d8ccd253f615f36fc
shamain <shamain.kolourbox@gmail.com>
2014-07-24 22:22:40 +0530

    employee-task

--- application/models/employee_task/employee_task_service.php
+++ application/models/employee_task/employee_task_service.php
@@ -9,7 +9,7 @@ class Employee_task_service extends CI_Model {
     public function get_employees_for_task($task_id) {
         $this->db->select('*');
         $this->db->from('employee_tasks');
-        $this->db->join('employee', 'employee.employee_code = employee_tasks.employee_code');
+        $this->db->join('employee', 'employee.employee_code = employee_tasks.employee_id');
         $this->db->where('employee_tasks.task_id', $task_id);
         $query = $this->db->get();
         return $query->result();
