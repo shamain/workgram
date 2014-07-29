565e0f4dd6938e62a37e20218364079d38d351c1
gayathma <gayathma3@gmail.com>
2014-07-29 15:29:15 +0530

    sas

--- application/controllers/skill_matrix/skill_matrix_controller.php
+++ application/controllers/skill_matrix/skill_matrix_controller.php
@@ -16,13 +16,16 @@ class Skill_matrix_controller extends CI_Controller {
 
             $this->load->model('skill_category/skill_category_model');
             $this->load->model('skill_category/skill_category_service');
+
+            $this->load->model('employee_skill/employee_skill_model');
+            $this->load->model('employee_skill/employee_skill_service');
         }
     }
-    
+
     function manage_skill_matrix() {
 
         $skill_category_service = new Skill_category_service();
-        $skill_service=new Skill_service();
+        $skill_service = new Skill_service();
         $data['heading'] = "Skill Matrix";
 
         $data['skills'] = $skill_service->get_all_skills();
@@ -32,7 +35,20 @@ class Skill_matrix_controller extends CI_Controller {
         $partials = array('content' => 'skill_matrix/skill_matrix_view');
         $this->template->load('template/main_template', $partials, $data);
     }
+
+    function add_employee_skill_matrix() {
+
+        $employee_skill_service = new Employee_skill_service();
+        $employee_skill_model = new Employee_skill_model();
+
+        $employee_skill_model->set_skill_code($this->input->post('skill_code', TRUE));
+        $employee_skill_model->set_employee_code($this->session->userdata('EMPLOYEE_CODE'));
+        $employee_skill_model->set_skill_cat_code($this->input->post('skill_cat_code', TRUE));
+        $employee_skill_model->set_del_ind('1');
+        $employee_skill_model->set_added_by($this->session->userdata('EMPLOYEE_CODE'));
+        $employee_skill_model->set_added_date(date("Y-m-d H:i:s"));
+
+        echo $employee_skill_service->add_new_employee_skill($employee_skill_model);
+    }
+
 }
-    
-    
-    
