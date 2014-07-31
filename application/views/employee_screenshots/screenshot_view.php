43a31ae2562db5c5cfabb773a56c4cefdc4ed39e
dahami <dahamiyh@ymail.com>
2014-07-31 20:20:07 +0100

    screenhot_view

---
+++ application/views/employee_screenshots/screenshot_view.php
@@ -0,0 +1,111 @@
+<div class="content">
+    <ul class="breadcrumb">
+        <div class="gallery"
+             <span class="semi-bold">
+                 Gallery
+            </span>
+    </div>
+    </ul>
+    
+    <div id="Filters" class="pull-left">
+        
+        <div class="btn-group">
+            <a class="btn dropdown-toggle btn-demo-space" data-toggle="dropdown" href="#">
+                <span class="anim150">
+                    Employee
+                </span>
+            </a>
+            <ul class="dropdown-menu">
+                <li class="active" data-dimension="" data-filter="all">
+                    <a href="#">
+                        All
+                    </a>
+                </li>
+                <li data-dimension="" data-filter="">
+                    <a href="#"
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="pull-right">
+        <div class="btn-group" data-toggle="buttons-radio">
+            <button id="ToList" class="btn btn-primary active" type="button">
+                <i class="fa fa-th-list"></i>
+            </button>
+             <button id="ToGrid" class="btn btn-primary" type="button">
+                <i class="fa fa-th-large"></i>
+            </button>
+           </div>
+   </div>
+    <div class="clearfix"></div>
+    
+    <div id="Parks" class="just list">
+        <div class="list_header">
+            <div id="sortByName" class="meta name active desc">
+                Employee
+                <span class="sort anim150 asc" data-order="desc" data-sort="data-name"></span>
+                <span class="sort anim150 desc active" data-order="asc" data-sort="data-name"></span>
+                    
+                      </div>
+            <div class="meta project">
+                Project
+            </div>
+
+        </div>
+        <div class="meta task">
+                Task
+            </div>
+        <div class="meta date">
+                Date
+            </div>
+
+        </div>
+    
+
+        </div>
+            
+       
+    
+    
+    
+    
+    </div>
+   
+
+
+
+
+
+
+</div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+</div>
