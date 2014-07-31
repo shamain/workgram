056b4d0966b4f7d8b9b4d0ab8cc29e2cb962fa74
dahami <dahamiyh@ymail.com>
2014-07-31 17:23:52 +0100

    screenshot_view

--- application/views/employee_screen_shots/screen_shot_view.php
+++ application/views/employee_screen_shots/screen_shot_view.php
@@ -1,4 +1,40 @@
 
+
+<div class="col-md-5">
+    <div class="form-group">
+     <label class="form-label">Employee</label>
+                               
+   </div>
+</div>
+<div class="col-md-6">
+     <div class="input-with-icon  right">                                       
+            <i class=""></i>
+   <select name="screen_shot" id="screen_shot" class="select2 form-control"  >
+                                    <option value=""></option>
+                                    
+                                </select>  
+
+                            </div>
+                        </div>
+ <div class="col-md-5">
+    <div class="form-group">
+     <label class="form-label">Date</label>
+                               
+   </div>
+</div>
+<div class="col-md-6">
+     <div class="input-with-icon  right">                                       
+            <i class=""></i>
+   <select name="screen_shot" id="screen_shot" class="select2 form-control"  >
+                                    <option value=""></option>
+                                    
+                                </select>  
+
+                            </div>
+                        </div>     
+
+
+                 
 <div class="superbox">
         <div class="superbox-list"> <img src="" data-img="" alt="" class="superbox-img"> </div>
         
@@ -43,5 +79,5 @@
 		</script>
                 
  
-?>
+
 
