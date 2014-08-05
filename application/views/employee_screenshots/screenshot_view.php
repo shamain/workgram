d0f821f9e6aae1d7fdd2f480637c1be54fd8e0fd
dahami <dahamiyh@ymail.com>
2014-08-05 16:48:20 +0100

    a

--- application/views/employee_screenshots/screenshot_view.php
+++ application/views/employee_screenshots/screenshot_view.php
@@ -29,6 +29,17 @@
 
             </ul>
         </div>
+         <div class="btn-group"> <a href="#" data-toggle="dropdown" class="btn dropdown-toggle btn-demo-space"> <span class="anim150">Task</span> <span class="caret"></span> </a>
+            <ul class="dropdown-menu">
+                <li class="active" data-filter="all" data-dimension="region"><a href="#">All</a></li>
+                <?php
+                foreach ($employees as $employee) {
+                    ?>
+                    <li data-filter = "alaska" data-dimension = "region"><a href = "#"><?php echo ucfirst($employee->employee_fname . ' ' . $employee->employee_lname); ?></a></li>
+                <?php }
+                ?>
+            </ul>
+        </div>
     </div>
 
     <div class="pull-right">
