9666caa643198403b5c2b8bd56c9e05dfa078e95
kaumadi <bhagyakaumadi@gmail.com>
2014-08-22 22:28:33 +0530

    manage

---
+++ application/views/wages/manage_wages_view.php
@@ -0,0 +1,13 @@
+<!--<div class="row-fluid">
+    <div id="work_snap_filters" class="pull-left">
+        <div class="btn-group"> <a href="#" data-toggle="dropdown" class="btn dropdown-toggle btn-demo-space"> <span class="anim150">Employee</span> <span class="caret"></span> </a>
+            <ul class="dropdown-menu">
+                <li class="active" data-filter="all" data-dimension="region"><a href="#">All</a></li>
+                <?php
+                foreach ($employees as $employee) {
+                    ?>
+                    <li data-filter = "alaska" data-dimension = "region"><a href = "#"><?php echo ucfirst($employee->employee_fname . ' ' . $employee->employee_lname); ?></a></li>
+                <?php }
+                ?>
+            </ul>
+        </div>-->
