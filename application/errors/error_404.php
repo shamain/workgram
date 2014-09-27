a562e99a6fe619e297fe9253ea97b04b6f40eae1
gayathma <gayathma3@gmail.com>
2014-09-27 12:59:16 +0530

    v

--- application/errors/error_404.php
+++ application/errors/error_404.php
@@ -26,32 +26,27 @@
 
 <!-- BEGIN BODY -->
 <body class="error-body no-top">
-    <div class="error-wrapper">
-        <div class="error-container  " style="height: 612px; left: 50%;">
-
-            <div class="error-main">
-                <div class="error-number">
-                    <?php echo $heading; ?>
-                </div>
-                <div class="span4 error-container error-description">
-                    We seem to have lost you in the clouds.
-                </div>
-                <div class="error-description-mini">
-                    The page your looking for is not here
-                </div>
-                <br />
-                <div class="row-fluid">
-                    <div class="input-with-icon span12">                                       
-                        <i class="icon-search"></i> 
-                        <input type="text" class="span12" id="form1Name" name="form1Name" />                                 
+    <div class="error-wrapper container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-offset-1 col-xs-10">
+                <div class="error-container" >
+                    <div class="error-main">
+                        <div class="error-number"> 404 </div>
+                        <div class="error-description" > We seem to have lost you in the clouds. </div>
+                        <div class="error-description-mini"> The page your looking for is not here </div>
+                        <br>
+                        <div class="row">
+                            <div class="input-with-icon col-md-12"> <i class="fa fa-search"></i>
+                                <input type="text" class="form-control" id="form1Name" name="form1Name">
+                            </div>
+                        </div>
+                        <br>
+                        <button class="btn btn-primary btn-cons" type="button">Search</button>
                     </div>
                 </div>
-                <br />
-                <button class="btn btn-primary btn-cons" type="button">Search</button>
-            </div>
 
+            </div>
         </div>
-        <div id="push"></div>
     </div>
     <div id="footer">
         <div class="span12 error-container">
